021b8afd60c5edba4463b23cb3da62faf6455788
Andrew Schueler <andrew.schueler@hotmail.com>
2017-04-22 12:22:22 -0400

    Added Examples Class

---
+++ src/Examples.java
@@ -0,0 +1,36 @@
+
+public class Examples {
+
+	// method to set up a ballot and cast votes
+
+	ElectionData Setup1 () {
+
+		ElectionData ED = new ElectionData();
+
+		// put candidates on the ballot
+
+		ED.addCandidate("gompei");
+		ED.addCandidate("husky");
+		ED.addCandidate("ziggy");
+
+		// cast votes
+
+		try {
+
+			ED.processVote("gompei", "husky", "ziggy");
+			ED.processVote("gompei", "ziggy", "husky");
+			ED.processVote("husky", "gompei", "ziggy");
+
+		} catch (Exception e) {}
+
+		return(ED);
+
+	}
+
+	// now run a test on a specific election
+	@Test
+	public void testMostFirstWinner () {
+		assertEquals ("gompei", Setup1().findWinnerMostFirstVotes());
+	}
+
+}
